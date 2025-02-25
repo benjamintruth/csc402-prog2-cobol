f9da37e8da7f5deb2d1468dde0128a92b63cd54f
St. Rien <steamshrimp@gmail.com>
2025-02-24 20:07:32 -0500

    init pt2

---
+++ prog2.cbl
@@ -0,0 +1,15 @@
+      ******************************************************************
+      * Author: Rien Sanning, Arnav Shah,
+      * Date: 24-02-25
+      * Purpose: Program 2, CSC 407
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROG2.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM PROG2.
